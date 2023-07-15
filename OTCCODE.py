87f6098ebf8596150d0560d8926307d252a82c88
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-07-15 17:47:59 -0400

    Update OTCCODE.py

--- OTCCODE.py
+++ OTCCODE.py
@@ -2,41 +2,112 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-# ... rest of your code ...
+def load_images(image_name):
+    img = Image.open(image_name)
+    return st.image(img, width=700)
 
-eligible_medications = set(disease_states[selection].keys())
-age = None
-is_eligible = True  # add this line
+c_image = 'Baner.png'
+load_images(c_image)
 
-for i in range(len(sheet)):
-    # ... rest of your code inside the loop ...
+disease_states = {
+    "GERD": {1:'Prilosec (omeprazole)', 2:'Nexium (esomeprazole)', 3:'Pepcid (famotidine)', 4:'Tums (calcium carbonate)', 5:'Milk of Magnesia (magnesium hydroxide)'},
+    "Allergies": {1:'Allegra 12 Hour (Fexofenadine)', 2:'Allegra 24 Hour (Fexofenadine)', 
+                  3:"Buckley's Jack and Jill Children's Formula (Diphenhydramine HCI / Phenylephrine HCI)",
+                  4:"Children's Allegra (Fexofenadine)", 5:'Chlor-Trimeton (Chlorpheniramine Maleate)',
+                  6:'Claritin (Loratadine)', 7:'Claritin Syrup (Loratadine)',
+                  8:'Dristan Long Lasting Menthol Spray (Oxymetazoline)', 
+                  9:'Dristan Long Lasting Nasal Mist (Oxymetazoline)',
+                  10:'Otrivin (Xylometazoline Hydrochloride)', 11:'Reactine (Cetirizine) 5 mg', 12:'Zyrtec (Cetrizine)', 13:'Benadryl'},
+    "Pain Control": {1:'Tylenol (acetaminophen)', 2:'Advil, Motrin (ibuprofen)', 
+                  3:"Aleve (naproxen)",
+                  4:"Aspirin", 5:'Lidoderm (topical lidocaine)',
+                  6:'Icy Hot (menthol + methyl salicylate)', 7:'Topical capsaicin',
+                  8:'Excedrin, Goodys Powder (acetaminophen + aspirin + caffeine)', 
+                  9:'Orajel (benzocaine oral topical)',
+                  10: "Biofreeze (menthol)", 11: "Tums (antacids)", 12: "Pepto Bismol (Bismuth Subsalicylate)"},
+    "Constipation": {1:'Metamucil (psyllium)', 2:'FiberCon, Fiber Lax (polycarbophil)', 3:'Citrucel (methylcellulose)', 4:'Dulcolax (bisacodyl)', 5:'Senokot (senna)', 6:'MiraLax (polyethylene glycol)',
+                     7:'Colace (docusate)', 8:'Citroma (magnesium citrate)', 9:'Kondremul (mineral oil)', 10:'Glycerin suppositories', 11:'Saline enemas'}
+}
 
-    if question == "Age condition":
-        # ... rest of your code in this condition ...
-        if eval(option1):
+    
+st.title("OTCRec: An Efficient Approach to Community Pharmacy Counseling")
+
+st.markdown(
+    "Welcome to the Patient Over The Counter Recommendation Program! This program will tell you which OTC medications you are eligible for based on your answers to some survey questions.  **Select the disease state in the sidebar to get started.**"
+)
+
+st.sidebar.markdown("**Please select the disease state that you would like to get recommendation on?**")
+options = [""] + list(disease_states.keys())
+selection = st.sidebar.selectbox("Disease State:", options)
+
+if selection == "Allergies":
+    st.sidebar.markdown("""
+    Allergic rhinitis usually arises from a trigger in the environment and resolves over time in the absence of the trigger.
+    Common symptoms include watery eyes, sneezing, runny nose, headache, and rash. Over-the-counter medications can help with these symptoms, 
+    but if they are persistent or become worse, medical attention is recommended.
+    """)
+if selection == "GERD":
+    st.sidebar.markdown("""
+    GERD, or gastroesophageal reflux disease, is when stomach acid flows back into the esophagus, causing symptoms like heartburn and difficulty swallowing. 
+    Over-the-counter medications such as antacids or acid reducers can help provide relief. If symptoms persist or worsen, it is recommended to seek medical attention for a proper diagnosis and potentially stronger medications. 
+    Consulting with a healthcare professional is important for personalized guidance and treatment options.        
+    """)
+if selection == "Pain Control":
+    st.sidebar.markdown("""
+    Pain management often involves the use of over-the-counter (OTC) medications to alleviate symptoms. These medications can help with headaches, muscle aches,
+    menstrual cramps, and minor injuries. However, it's important to carefully follow the instructions, recommended dosage, and duration of use provided on the packaging.
+    If pain persists or becomes severe, medical attention is recommended.
+    """)
+if selection == "Constipation":
+    st.sidebar.markdown("""
+    Constipation is a common condition that affects the digestive system - patients have difficulty passing stool or are unable to have
+    regular bowel movements. Luckily, there are several products that are available over the counter to treat this condition. Each type of
+    medication can provide relief for patients,and there are many different formulation options as well. It should be noted that
+    these over-the-counter options are only meant to treat short-term constipation.
+    Some cases of constipation may require prescription medication or further medical attention.
+    """)
+if selection:
+    sheet = pd.read_excel("OTCRecommendations.xlsx", sheet_name = selection)
+    sheet.columns = sheet.columns.str.strip()
+    eligible_medications = set(disease_states[selection].keys())
+    age = None
+
+    for i in range(len(sheet)):
+        question = sheet.loc[i, "Question"]
+        option1 = sheet.loc[i, "Option 1"]
+        option2 = sheet.loc[i, "Option 2"]
+        options = str(sheet.loc[i, "options"])  # Cast to string to avoid errors in case the value is not a string
+        
+        if question == "Please enter your age:":
+            age = st.selectbox(question, list(range(1,101)))
+            continue
+
+        if question == "Age condition":
+            option1 = option1.replace("Age", str(age))
+            if eval(option1):
+                if options.lower() == "none":
+                    st.write("Based on your responses you are not eligible for over the counter medications. Please consult a healthcare provider.")
+                    eligible_medications = set()
+                    break
+                else:
+                    option_numbers = list(map(int, options.split(',')))
+                    eligible_medications.intersection_update(option_numbers)
+            continue
+
+        response = st.radio(question, options = [option1, option2], index=1)  # index=1 to set "Option 2" as default
+        
+        if response == option1:
             if options.lower() == "none":
-                # st.write("Based on your responses you are not eligible for over the counter medications. Please consult a healthcare provider.")
-                is_eligible = False  # change this line
+                st.write("Based on your responses you are not eligible for over the counter medications. Please consult a healthcare provider or contact your local pharmacy.")
                 eligible_medications = set()
                 break
             else:
-                # ... rest of your code in this condition ...
-            continue
-    response = st.radio(question, options = [option1, option2], index=1)  # index=1 to set "Option 2" as default
+                option_numbers = list(map(int, options.split(',')))
+                eligible_medications.intersection_update(option_numbers)
 
-    if response == option1:
-        if options.lower() == "none":
-            # st.write("Based on your responses you are not eligible for over the counter medications. Please consult a healthcare provider or contact your local pharmacy.")
-            is_eligible = False  # change this line
-            eligible_medications = set()
-            break
-        else:
-            # ... rest of your code in this condition ...
-
-# this block of code outside the loop:
-if eligible_medications:
-    st.write("Based on your responses, you are eligible for the following medications:")
-    for num in eligible_medications:
-        st.write(disease_states[selection][num])
-elif not is_eligible:  # check if the user is not eligible
-    st.write("Based on your responses you are not eligible for over the counter medications. Please consult a healthcare provider or contact your local pharmacy.")
+    if eligible_medications:
+        st.write("Based on your responses, you are eligible for the following medications:")
+        for num in eligible_medications:
+            st.write(disease_states[selection][num])
+    else:
+        st.write("Based on your responses you are not eligible for over the counter medications. Please consult a healthcare provider or contact your local pharmacy. ")
