364ccfb6eafd770f86929fecbb75945c688a83a4
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-07-14 21:43:41 -0400

    Update OTCCODE.py

--- OTCCODE.py
+++ OTCCODE.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
 
 #OTC_df = pd.read_excel("otc.xlsx")
 
@@ -10,3 +11,10 @@ st.markdown(
     "Welcome to the OTC Recommendation Program! This program will tell you which OTC medications you are eligible for based on your answers to some survey questions.  **Select the disease state in the sidebar to get started.**"
 )
 
+
+def load_images(image_name):
+    img = Image.open(image_name)
+    return st.image(img, width=800)
+
+c_image = 'diab.png'
+load_images(c_image)
