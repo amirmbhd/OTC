05b42f754fa42d5e8ce7e127a666413c4f3a7ecf
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2023-07-15 17:42:23 -0400

    Update OTCCODE.py

--- OTCCODE.py
+++ OTCCODE.py
@@ -2,6 +2,33 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
+def load_images(image_name):
+    img = Image.open(image_name)
+    return st.image(img, width=700)
+
+c_image = 'Baner.png'
+load_images(c_image)
+
+disease_states = {
+    "GERD": {1:'Prilosec (omeprazole)', 2:'Nexium (esomeprazole)', 3:'Pepcid (famotidine)', 4:'Tums (calcium carbonate)', 5:'Milk of Magnesia (magnesium hydroxide)'},
+    "Allergies": {1:'Allegra 12 Hour (Fexofenadine)', 2:'Allegra 24 Hour (Fexofenadine)', 
+                  3:"Buckley's Jack and Jill Children's Formula (Diphenhydramine HCI / Phenylephrine HCI)",
+                  4:"Children's Allegra (Fexofenadine)", 5:'Chlor-Trimeton (Chlorpheniramine Maleate)',
+                  6:'Claritin (Loratadine)', 7:'Claritin Syrup (Loratadine)',
+                  8:'Dristan Long Lasting Menthol Spray (Oxymetazoline)', 
+                  9:'Dristan Long Lasting Nasal Mist (Oxymetazoline)',
+                  10:'Otrivin (Xylometazoline Hydrochloride)', 11:'Reactine (Cetirizine) 5 mg', 12:'Zyrtec (Cetrizine)', 13:'Benadryl'},
+    "Pain Control": {1:'Tylenol (acetaminophen)', 2:'Advil, Motrin (ibuprofen)', 
+                  3:"Aleve (naproxen)",
+                  4:"Aspirin", 5:'Lidoderm (topical lidocaine)',
+                  6:'Icy Hot (menthol + methyl salicylate)', 7:'Topical capsaicin',
+                  8:'Excedrin, Goodys Powder (acetaminophen + aspirin + caffeine)', 
+                  9:'Orajel (benzocaine oral topical)',
+                  10: "Biofreeze (menthol)", 11: "Tums (antacids)", 12: "Pepto Bismol (Bismuth Subsalicylate)"},
+    "Constipation": {1:'Metamucil (psyllium)', 2:'FiberCon, Fiber Lax (polycarbophil)', 3:'Citrucel (methylcellulose)', 4:'Dulcolax (bisacodyl)', 5:'Senokot (senna)', 6:'MiraLax (polyethylene glycol)',
+                     7:'Colace (docusate)', 8:'Citroma (magnesium citrate)', 9:'Kondremul (mineral oil)', 10:'Glycerin suppositories', 11:'Saline enemas'}
+}
+
 # ... rest of your code ...
 
 eligible_medications = set(disease_states[selection].keys())
@@ -9,33 +36,7 @@ age = None
 is_eligible = True  # add this line
 
 for i in range(len(sheet)):
-    question = sheet.loc[i, "Question"]
-    option1 = sheet.loc[i, "Option 1"]
-    option2 = sheet.loc[i, "Option 2"]
-    options = str(sheet.loc[i, "options"])  # Cast to string to avoid errors in case the value is not a string
-
-    if question == "Please enter your age:":
-        age = st.selectbox(question, list(range(1,101)))
-    elif question == "Age condition":
-        option1 = option1.replace("Age", str(age))
-        if eval(option1):
-            if options.lower() == "none":
-                is_eligible = False  # change this line
-                eligible_medications = set()
-                break
-            else:
-                option_numbers = list(map(int, options.split(',')))
-                eligible_medications.intersection_update(option_numbers)
-    else:
-        response = st.radio(question, options = [option1, option2], index=1)  # index=1 to set "Option 2" as default
-        if response == option1:
-            if options.lower() == "none":
-                is_eligible = False  # change this line
-                eligible_medications = set()
-                break
-            else:
-                option_numbers = list(map(int, options.split(',')))
-                eligible_medications.intersection_update(option_numbers)
+    # ... rest of your code inside the loop ...
 
 # this block of code outside the loop:
 if eligible_medications:
